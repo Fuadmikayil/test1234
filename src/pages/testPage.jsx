4a57a544c918654e03e6ab5595c6506f77484040
Fuadmikayil <mikayilov2006@gmail.com>
2026-05-31 12:10:16 +0400

    test

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from "../commponents/Nav.jsx"
+import BigCard from "../commponents/BigCard.jsx"
 
 const TestPage = () => {
   return (
      <>
       <Nav/>
+      <BigCard/>
     </>
   )
 }
